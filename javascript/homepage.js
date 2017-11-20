cadd8561013e6f4badfad911a0f946e11403f624
christine zhong <zjzhong@sfu.ca>
2017-11-20 14:22:46 -0800

    added java, for the scrollify effect and also the anchor point for the navi at the left side and also changd the style for the navigation bar

---
+++ javascript/homepage.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $.scrollify({
+        section : ".home-section",
+        before: function(activeIndex, sections) {
+            var currentSection = sections[activeIndex].attr("data-section-name");
+            var activeCircle = $(".home-section-nav").find("." + currentSection);
+            $(".home-section-nav").find(".active").removeClass("active");
+            activeCircle.addClass("active");
+        }
+    });
+})
+
+$(".home-section-nav a").on("click", function(e) {
+    e.preventDefault();
+    console.log(e)
+})
+
+$(window).on("keypress", function(e) {
+    if (e.keyCode === 32) {
+        e.preventDefault();
+        $.scrollify.next();
+    }
+})
