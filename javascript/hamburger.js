18461d534f61f59ca39813a09725efbfcc7b69de
christine zhong <zjzhong@sfu.ca>
2017-11-21 00:55:58 -0800

    changed menu bar for homepage

---
+++ javascript/hamburger.js
@@ -0,0 +1,11 @@
+$(doocument).ready(function () {
+    
+    $(".mobile-nav-trigger").on("click", function() {
+        if ($(".mobile-nav").hasClass("active")) {
+            $(".mobile-nav").removeClass("active");
+        }
+        else {
+            $(".mobile-nav").addClass("active");
+        }
+    })
+})
