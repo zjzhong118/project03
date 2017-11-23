19ba67a41f803e7b33a703ee3eec6b64a7f6e6b5
christine zhong <zjzhong@sfu.ca>
2017-11-23 12:48:13 -0800

    fixed the hamburger menu, also fixed the grid for the animation page

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
     $.scrollify({
-        section : ".home-section",
+        section : ".scroll-section",
         interstitialSection: "footer",
+        touchScroll: false,
         before: function(activeIndex, sections) {
             var currentSection = sections[activeIndex].attr("data-section-name");
-            var activeCircle = $(".home-section-nav").find("." + currentSection);
-            $(".home-section-nav").find(".active").removeClass("active");
+            var activeCircle = $(".scroll-section-nav").find("." + currentSection);
+            $(".scroll-section-nav").find(".active").removeClass("active");
             activeCircle.addClass("active");
         }
     });
 })
 
-$(".home-section-nav a").on("click", function(e) {
+$(".scroll-section-nav a").on("click", function(e) {
     e.preventDefault();
     console.log(e)
 })
