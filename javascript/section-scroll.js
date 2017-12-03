da8add59464072a5736631a102b73780441d8172
zjzhong118 <zjzhong@sfu.ca>
2017-12-02 19:28:28 -0800

    final update

--- javascript/section-scroll.js
+++ javascript/section-scroll.js
@@ -1,25 +1,1 @@
-$(document).ready(function() {
-    $.scrollify({
-        section : ".scroll-section",
-        interstitialSection: "footer",
-        touchScroll: false,
-        before: function(activeIndex, sections) {
-            var currentSection = sections[activeIndex].attr("data-section-name");
-            var activeCircle = $(".scroll-section-nav").find("." + currentSection);
-            $(".scroll-section-nav").find(".active").removeClass("active");
-            activeCircle.addClass("active");
-        }
-    });
-})
-
-$(".scroll-section-nav a").on("click", function(e) {
-    e.preventDefault();
-    console.log(e)
-})
-
-$(window).on("keypress", function(e) {
-    if (e.keyCode === 32) {
-        e.preventDefault();
-        $.scrollify.next();
-    }
-})
+$(document).ready(function() {    $.scrollify({        section : ".scroll-section",        setHeights: false,        interstitialSection: "footer",        touchScroll: false,        before: function(activeIndex, sections) {            var currentSection = sections[activeIndex].attr("data-section-name");            var activeCircle = $(".scroll-section-nav").find("." + currentSection);            $(".scroll-section-nav").find(".active").removeClass("active");            activeCircle.addClass("active");        }    });})$(".scroll-section-nav a").on("click", function(e) {    e.preventDefault();    console.log(e)})$(window).on("keypress", function(e) {    if (e.keyCode === 32) {        e.preventDefault();        $.scrollify.next();    }})
